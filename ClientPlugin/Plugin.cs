5ae20ed2a87bfedafe16dac031e54e331c951c59
Space <unclearspace@gmail.com>
2026-08-18 15:36:22 +0100

    Fix csproj

--- ClientPlugin/Plugin.cs
+++ ClientPlugin/Plugin.cs
@@ -5,7 +5,7 @@ using HarmonyLib;
 using VRage.Plugins;
 
 // Set the assembly version manually if compiled by Pulsar (it won't create what was in AssemblyInfo.cs before)
-#if !DEV_BUILD
+#if PULSAR
 using System.Reflection;
 
 [assembly: AssemblyVersion("1.0.17.0")]
